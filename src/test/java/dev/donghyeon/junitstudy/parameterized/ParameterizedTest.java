ed2d84d5c7b6949588ff358230c98dc766b43bb3
Donghyeon Min <mindonghyeon890@gmail.com>
2020-12-04 08:55:45 +0900

    추가추가

---
+++ src/test/java/dev/donghyeon/junitstudy/parameterized/ParameterizedTest.java
@@ -0,0 +1,16 @@
+package dev.donghyeon.junitstudy.parameterized;
+
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.util.StringUtils;
+
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+public class ParameterizedTest {
+
+//    @ParameterizedTest
+//    @ValueSource(strings = {"racecar", "radar", "able was I ere I saw elba"})
+//    void palindromes(String candidate) {
+//        assertTrue(StringUtils.isPalindrome(candidate));
+//    }
+}
